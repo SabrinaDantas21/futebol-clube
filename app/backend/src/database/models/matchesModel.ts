13d5e95a7484ca25e98d1177477f16a77363cc0c
SabrinaDantas21 <brinahdantas56@gmail.com>
2024-10-15 17:39:42 -0300

    requisito 13 feito, com a migration de matches e a model

---
+++ app/backend/src/database/models/matchesModel.ts
@@ -0,0 +1,56 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class Matches extends Model<InferAttributes<Matches>,
+InferCreationAttributes<Matches>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    homeTeamId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    homeTeamGoals: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    awayTeamId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    awayTeamGoals: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'matches',
+    timestamps: false,
+  },
+);
+
+export default Matches;
